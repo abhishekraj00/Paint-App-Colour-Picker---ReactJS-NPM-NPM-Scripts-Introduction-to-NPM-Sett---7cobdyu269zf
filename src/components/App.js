982060b106c8680cdec1c98574501a91668b51e5
Abhishek Raj Gupta <110536209+abhishekraj00@users.noreply.github.com>
2022-11-19 12:19:39 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,37 @@
 import React, { useState } from "react";
 import '../styles/App.css';
+import "../styles/Child.css";
 import Selection from './Selection';
 import ColourSelector from './ColourSelector';
 
+
 const colourConfig = [{
-    key: 'blue',
-    label: 'Blue',
-    classname: 'btn-blue',
-    background: 'rgb(34, 193, 195)'
-  }, {
-    key: 'orange',
-    label: 'Orange',
-    classname: 'btn-orange',
-    background: 'rgb(221, 112, 18)'
-  }, {
-    key: 'green',
-    label: 'Green',
-    classname: 'btn-green',
-    background: 'rgb(44, 209, 88)'
-  }
+  key: 'blue',
+  label: 'Blue',
+  classname: 'btn-blue',
+  background: 'rgb(34, 193, 195)'
+}, {
+  key: 'orange',
+  label: 'Orange',
+  classname: 'btn-orange',
+  background: 'rgb(221, 112, 18)'
+}, {
+  key: 'green',
+  label: 'Green',
+  classname: 'btn-green',
+  background: 'rgb(44, 209, 88)'
+}
 ]
 
 const title = 'Select the gradient and then the Box to change the color';
 
 const App = () => {
   let [nextBackground, selectNextBackground] = useState({ background: "" })
-  
   const applyColor = (updateSelectionStyle) => {
     updateSelectionStyle(nextBackground)
   }
-
+  console.log(nextBackground);
   return (
-
-    <>
-    
     <div id="master">
       <h5 className="heading">{title}</h5>
 
@@ -51,8 +49,6 @@ const App = () => {
         }
       </div>
     </div >
-   
-    </>
   )
 }
 
